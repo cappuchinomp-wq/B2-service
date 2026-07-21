a4e4525c044a6be9a0058a9f3c3d77ff38cfa563
cappuchinomp-wq <cappuchino.mp@gmail.com>
2026-07-21 15:11:17 +0700

    Update App rev1

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ const apiService = {
     return this.request(CONFIG.API_URL, {
       method: "POST",
       headers: {
-        "Content-Type": "text/plain;charset=utf-8"
+        "Content-Type": "text/plain;charset=UTF-8"
       },
       body: JSON.stringify(payload)
     });
